69a2ed3dab521f4bbc4a423d5a7d46b267e56a24
slynne3 <muld0079@algonquinlive.com>
2017-12-01 18:27:36 -0500

    Start Activity

---
+++ app/src/main/java/com/example/staceylm/foodnutritionactivity/StartActivity.java
@@ -0,0 +1,32 @@
+package com.example.staceylm.foodnutritionactivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        // Button bActivityTracker = findViewById(R.id.bActivityTracker);
+        //Button bFoodTracker = (Button) findViewById(R.id.foodButton);
+        // Button bThermostat = findViewById(R.id.bThermostat);
+        // Button bAutomobile = findViewById(R.id.bAutomobile);
+
+//        bFoodTracker.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//               Intent foodTracker = new Intent(getApplicationContext(), MainActivity.class);
+//               // startActivity(foodTracker);
+//                startActivityForResult(foodTracker, 10);
+//            }
+//        });
+//
+//    }
+    }
+}
