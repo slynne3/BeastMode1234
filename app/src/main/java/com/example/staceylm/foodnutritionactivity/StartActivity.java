df1a6188a60fbe13097a374b2e08991e5b96b764
slynne3 <muld0079@algonquinlive.com>
2017-12-09 14:16:07 -0500

    Start Activity

--- app/src/main/java/com/example/staceylm/foodnutritionactivity/StartActivity.java
+++ app/src/main/java/com/example/staceylm/foodnutritionactivity/StartActivity.java
@@ -1,32 +1,33 @@
 package com.example.staceylm.foodnutritionactivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class StartActivity extends Activity {
+public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        Button bActivityTracker = findViewById(R.id.bActivityTracker);
+        // Button bActivityTracker = findViewById(R.id.bActivityTracker);
         Button bFoodTracker = (Button) findViewById(R.id.bFoodTracker);
-        Button bThermostat = findViewById(R.id.bThermostat);
-        Button bAutomobile = findViewById(R.id.bAutomobile);
+        // Button bThermostat = findViewById(R.id.bThermostat);
+        // Button bAutomobile = findViewById(R.id.bAutomobile);
 
         bFoodTracker.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-               Intent foodTracker = new Intent(getApplicationContext(), Thermostat.class);
-              //  startActivity(foodTracker);
-                startActivityForResult(foodTracker, 10);
+            public void onClick(View view) {
+
+               Intent f = new Intent(StartActivity.this, MainActivity.class);
+               startActivityForResult(f, 100);
             }
-       });
+        });
 
     }
 
 }
+
